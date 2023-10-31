2d14e25525030e0c8a0dabc3e148695a2ce11412
Ezzy180 <134999590+Ezzy180@users.noreply.github.com>
2023-10-31 22:05:59 +0300

    Add files via upload

--- Account.java
+++ Account.java
@@ -3,6 +3,19 @@ public class Account {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("SAVINGS ACCOUNT DETAILS");
+		SavingsAccount sac = new SavingsAccount(1001, "Ann Kamau", "070200000", "test@gmail.com", 1000);
+		sac.accountholder();
+		sac.deposit(500);
+		sac.displayBalance();
+		
+		System.out.print("\n");
+		
+		System.out.println("CURRENT ACCOUNT DETAILS");
+		CurrentAccount cac = new CurrentAccount(1001, "Peter Smith", "08100000", "admin@gmail.com", 0);
+		cac.accountholder();
+		cac.deposit(300);
+		cac.displayBalance();
 
 	}
 
